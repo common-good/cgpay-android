43a1d3164b937c616ecbe921a1cc204b3bfa3854
G. Blake Meike <blake.meike@gmail.com>
2012-03-08 16:48:10 -0800

    Add the background image

--- POS/src/com/commongoodbank/android/pos/IdleActivity.java
+++ POS/src/com/commongoodbank/android/pos/IdleActivity.java
@@ -1,10 +1,9 @@
 package com.commongoodbank.android.pos;
 
-import com.commongoodbank.android.pos.R;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class IdleActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
